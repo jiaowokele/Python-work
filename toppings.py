7da1e7a6aafff67ae983c74f60fcc6b55d933885
TAYep <553916792@qq.com>
2019-04-26 22:53:28 +0800

    Chapter 5

---
+++ toppings.py
@@ -0,0 +1,9 @@
+requested_topping = 'mushrooms'
+if  requested_topping != 'anchovies':
+    print("Hold the anchovies")
+
+answer = 17
+if answer != 42 :
+    print("That is not the correct answer. Please try again!")
+age = 17
+print(age > 66)
